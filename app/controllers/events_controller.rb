80deae931ee06318f6a5caf258305aa1f86d2a07
Saioa Arostegui <saioarostegi@gmail.com>
2020-02-24 17:07:58 +0100

    cloudinary should be up and running now

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,18 @@ class EventsController < ApplicationController
   def index
     puts "Hello +u loser"
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :photo)
+  end
 end
