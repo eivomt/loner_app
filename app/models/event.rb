6593e2db3b042a443382cbb032989616c8cb717c
Eivind Omtveit <e.omtveit@gmail.com>
2020-02-25 18:22:35 +0100

    slight seed problem

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: "User", foreign_key: :user_id
+  belongs_to :creator, class_name: "User", foreign_key: :creator_id
   has_many :event_users, dependent: :destroy
   has_many :users, through: :event_users
   has_one_attached :photo
