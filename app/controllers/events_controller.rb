ff1ccbcd275dff77d5afd767695c60c3ed0f3171
Saioa Arostegui <saioarostegi@gmail.com>
2020-02-27 16:45:01 +0100

    friends list and carousel partial created for the index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,6 +13,35 @@ class EventsController < ApplicationController
         image_url: helpers.asset_url('marker-stroked-15.svg')
       }
     end
+
+    @user_friends_id = []
+    @array_of_events_instances = []
+    @events_id = []
+    @array_of_friends = []
+
+    current_user.event_users.each do |event|
+      @events_id << event.event_id
+        @events_id.each do |id|
+          e = Event.find(id)
+          @array_of_events_instances << e
+
+          @array_of_events_instances.each do |event_instances|
+            u = event_instances.user_ids
+            u.each do |user_id|
+              user = User.find(user_id)
+              if current_user == user
+
+              elsif @array_of_friends.include?(user)
+              else
+                @array_of_friends << user
+              end
+            end
+          end
+        end
+        # p "hello there user friend id #{@user_friends_id}"
+        # p "hello there array of instance #{@array_of_users_instances}"
+    end
+    @array_of_friends
   end
 
   def new
