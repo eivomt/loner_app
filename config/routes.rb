2982da8c0d677c71c310e696150b2561b0bba6bb
Saioa Arostegui <saioarostegi@gmail.com>
2020-02-28 16:34:26 +0100

    Fix ws configuration

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
   mount ActionCable.server => '/cable'
 
-
-
   devise_for :users
   root to: 'events#index'
   resources :events, only: [ :new, :create, :show, :edit, :update, :destroy ] do
@@ -11,9 +9,7 @@ Rails.application.routes.draw do
     resources :comments, only: [ :create ]
   end
 
-
   resources :event_users, only: :destroy
   resources :users, only: :show
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
