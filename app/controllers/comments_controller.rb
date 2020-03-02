c9035e0a5017d1fa0b814d8c321c44cb1eacaabe
Eivind Omtveit <e.omtveit@gmail.com>
2020-03-02 16:26:08 +0100

    wall working again

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
+
     @comment = Comment.new(comment_params)
     @comment.event = @event
     @comment.user = current_user
-    @comment.save!
+    @comment.save
 
     # and then is rendering comments/create.js.erb
   end
@@ -14,5 +15,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:body)
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :event_users
-  has_many :events, dependent: :destroy
+  has_many :event_users, dependent: :destroy
+  has_many :events, through: :event_users
+
   has_one_attached :photo
+
   # validates :username, :age, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
