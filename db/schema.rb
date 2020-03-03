d10b5304bfb7e4b3fc7cf7c590964c2c4772f095
Saioa Arostegui <saioarostegi@gmail.com>
2020-03-03 18:14:23 +0100

    following friends system is uuuuuppppp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_133727) do
+ActiveRecord::Schema.define(version: 2020_03_03_163338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,16 @@ ActiveRecord::Schema.define(version: 2020_02_27_133727) do
     t.float "latitude"
     t.float "longitude"
     t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "following_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true
+    t.index ["following_id"], name: "index_follows_on_following_id"
   end
 
   create_table "users", force: :cascade do |t|
