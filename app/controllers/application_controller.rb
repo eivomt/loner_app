83ee3a27171b4c41b558717b6bf28ceba4149ef7
Saioa Arostegui <saioarostegi@gmail.com>
2020-03-03 10:20:48 +0100

    out changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_filter :strip_www
-
-  def strip_www
-    if request.env["HTTP_HOST"] == "www.plus-u.best"
-      redirect_to "http://plus-u.best/"
-    end
-  end
-
-  def default_url_options
-  { host: ENV["DOMAIN"] || "localhost:3000" }
-  end
 end
