605ac97764e8c8434a17c79198b5008940f71d0e
Eivind Omtveit <e.omtveit@gmail.com>
2020-03-04 13:57:34 +0100

    logo animation and scroll indicator animations

---
+++ app/javascript/plugins/hero_animation.js
@@ -0,0 +1,62 @@
+import anime from 'animejs';
+import Rails from '@rails/ujs';
+
+console.log('hello hero animation')
+const initHeroAnimation = () => {
+  const hero = document.querySelector(".hero-container")
+  const plus = document.querySelector(".hero-plus")
+  const smallArrow = document.querySelector(".small_arrow")
+  // const bigArrow = document.querySelector(".big_arrow")
+
+  if (hero) {
+    const logoTimeline = anime.timeline({
+      easing : 'easeInOutExpo',
+      loop: true,
+      direction: 'alternate',
+      duration: '2000'
+    });
+
+    logoTimeline
+    .add({
+      targets: plus,
+      duration: '500'
+    })
+
+    .add({
+      targets: '.square',
+      duration: '1000',
+      easing: 'spring(1, 80, 10, 0)',
+      rotate: '180'
+    })
+
+    .add({
+      targets: plus,
+      duration: '1000',
+      // direction: 'alternate',
+      // easing : 'easeInOutExpo',
+      // loop: true,
+      // easing: 'spring(1, 80, 10, 0)',
+      fill: '#68E184'
+    }, '-=2000')
+
+    .add({
+      targets: smallArrow,
+      translateY: '100',
+      easing: 'spring(1, 80, 10, 0)',
+      stroke: '#68E184',
+      duration: '1000'
+    }, '-=1600')
+
+    .add({
+      targets: plus,
+      duration: '500'
+    }, '-=0');
+
+
+  } else {
+    console.log('you fail')
+    return
+  }
+};
+
+export { initHeroAnimation };
