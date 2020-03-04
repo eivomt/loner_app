300758ce22c0e125ebbfc962398590e4a63bf270
TorfinnWeisser <torfinn.weisser@vg.no>
2020-03-04 22:13:58 +0100

    simpel fade logo

---
+++ app/javascript/plugins/hero_fade.js
@@ -0,0 +1,11 @@
+console.log('hello fader')
+const initFadeOnScroll = () => {
+  const heroLogo = document.getElementById("hero-logo")
+  window.addEventListener('scroll', function() {
+    heroLogo.style.opacity = 1 - window.pageYOffset/400+''
+    heroLogo.style.top = +window.pageYOffset+'px'
+    heroLogo.style.backgroundPositionY = - window.pageYOffset/2+'px'
+  })
+}
+
+export { initFadeOnScroll };
