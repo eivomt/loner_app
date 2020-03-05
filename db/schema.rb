7ec2bc542216b2d3dd8c50f340a86a3c4628db80
TorfinnWeisser <torfinn.weisser@vg.no>
2020-03-05 10:49:16 +0100

    user show fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_135840) do
+ActiveRecord::Schema.define(version: 2020_03_04_130237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,11 +42,6 @@ ActiveRecord::Schema.define(version: 2020_03_04_135840) do
     t.boolean "read", default: false
     t.index ["event_id"], name: "index_comment_alerts_on_event_id"
     t.index ["user_id"], name: "index_comment_alerts_on_user_id"
-  end
-
-  create_table "commentalerts", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "comments", force: :cascade do |t|
