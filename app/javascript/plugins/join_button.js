db59ce648f6f765559d93d325adcfe46c516436c
Eivind Omtveit <e.omtveit@gmail.com>
2020-03-06 04:47:05 +0100

    yees

--- app/javascript/plugins/join_button.js
+++ app/javascript/plugins/join_button.js
@@ -96,19 +96,20 @@ const initJoinButton = () => {
 
 const feedbackAnimation = () => {
 
-  let updatingAnim = false
+  // window.scrollTo(0,0)
 
-  if (updatingAnim) {
-    return
-  }
+  // let updatingAnim
+  // if (updatingAnim) {
+  //   return
+  // }
 
-  updatingAnim = true
+  // updatingAnim = true
 
   const avatars = document.querySelectorAll(".feedback-avatar")
   const plus = document.querySelector(".show-plus")
   const plusBox = document.querySelector(".plus-box-show")
   const me = document.querySelector(".feedback-avatar-me")
-  const allAvatars = avatars + me
+  const newEventButton = document.querySelector(".add-button-wrapper, input")
   const circlePath = "M 64 32 C 64 36.418 63.105 40.627 61.485 44.456 C 59.866 48.284 57.523 51.732 54.627 54.627 C 51.732 57.523 48.284 59.866 44.456 61.485 C 40.627 63.105 36.418 64 32 64 C 26.109 64 20.59 62.408 15.849 59.631 C 11.108 56.854 7.146 52.892 4.369 48.151 C 1.592 43.41 0 37.891 0 32 C 0 28.465 0.573 25.065 1.631 21.886 C 2.69 18.706 4.233 15.749 6.174 13.101 C 8.115 10.453 10.453 8.115 13.101 6.174 C 15.749 4.233 18.706 2.69 21.886 1.631 C 25.065 0.573 28.465 0 32 0 C 35.535 0 38.935 0.573 42.114 1.631 C 45.294 2.69 48.251 4.233 50.899 6.174 C 53.547 8.115 55.885 10.453 57.826 13.101 C 59.767 15.749 61.31 18.706 62.369 21.886 C 63.427 25.065 64 28.465 64 32 L 64 32"
   const plusPath = "M 63 35 C 58.333 35 53.667 35 49 35 C 44.333 35 39.667 35 35 35 C 35 39.667 35 44.333 35 49 C 35 53.667 35 58.333 35 63 C 32.667 63 30.333 63 28 63 C 28 53.667 28 44.333 28 35 C 18.667 35 9.333 35 0 35 C 0 32.5 0 30 0 27.5 C 9.333 27.5 18.667 27.5 28 27.5 C 28 18.333 28 9.167 28 0 C 30.333 0 32.667 0 35 0 C 35 0 35 0 35 0 C 35 9.167 35 18.333 35 27.5 C 37.333 27.5 39.667 27.5 42 27.5 C 44.333 27.5 46.667 27.5 49 27.5 C 51.333 27.5 53.667 27.5 56 27.5 C 58.333 27.5 60.667 27.5 63 27.5 L 63 35"
   const targets = [avatars, plus, plusBox, me]
@@ -119,17 +120,17 @@ const feedbackAnimation = () => {
 
   timeline
   .add({
+    targets: newEventButton,
+    opacity: 0,
+    easing: "easeInExpo",
+    duration: 200
+  })
+
+  .add({
   duration: 1000,
   targets: ".feedback-container",
   top: "200vh",
   easing: "easeInOutExpo"
-  })
-
-  .add ({
-    duration: 500,
-    targets: avatars,
-    translateX: 131,
-    delay: anime.stagger(100)
   })
 
   .add({
@@ -233,7 +234,6 @@ const feedbackAnimation = () => {
     targets: plusBox,
     duration: 1,
     translateY: 0,
-    // top: '11.5%'
   })
 
   .add({
@@ -245,12 +245,19 @@ const feedbackAnimation = () => {
   })
 
   .add({
+    targets: newEventButton,
+    opacity: 1,
+    easing: "easeInExpo",
+    duration: 200
+  }, '-=500')
+
+  .add({
     targets: '.square',
     duration: 1,
     rotate: '-630'
   });
 
-  updatingAnim = false
+  // updatingAnim = false
 };
 
 
